fc3a63092804bff59de092835d6160ebaa6a5a4e
jek730 <jejek730@gmail.com>
2024-09-06 20:03:24 +0900

    ReservationController 추가

---
+++ src/main/java/xyz/sangdam/reservation/controllers/ReservationController.java
@@ -0,0 +1,12 @@
+package xyz.sangdam.reservation.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name="Reservation", description = "예약 API")
+@RestController
+@RequiredArgsConstructor
+public class ReservationController {
+
+}
