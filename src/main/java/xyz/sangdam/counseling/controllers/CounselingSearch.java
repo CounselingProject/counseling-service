ca678b6296977de1b3d8b5dc0b8e68464fe43fb3
sswum <l5450won@naver.com>
2024-09-12 18:21:35 +0900

    상담프로그램 조회 작업중

--- src/main/java/xyz/sangdam/counseling/controllers/CounselingSearch.java
+++ src/main/java/xyz/sangdam/counseling/controllers/CounselingSearch.java
@@ -1,8 +1,23 @@
 package xyz.sangdam.counseling.controllers;
 
 import lombok.Data;
+import xyz.sangdam.counseling.constants.CounselingType;
 import xyz.sangdam.global.CommonSearch;
 
 @Data
 public class CounselingSearch extends CommonSearch {
+    private int page = 1;
+    private int limit = 5;
+
+    private String sopt; // 검색 조건
+    private String skey; // 검색 키워드
+
+    // 상담명으로 목록을 조회할거냐 타입으로 할거냐 ..
+
+    private CounselingType counselingType;
+    private String counselingName;
+
+    private String sort; // 정렬 조건
+
+
 }
--- src/main/java/xyz/sangdam/counseling/services/CounselingInfoService.java
+++ src/main/java/xyz/sangdam/counseling/services/CounselingInfoService.java
@@ -2,8 +2,10 @@ package xyz.sangdam.counseling.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import xyz.sangdam.counseling.constants.CounselingType;
 import xyz.sangdam.counseling.controllers.CounselingSearch;
 import xyz.sangdam.counseling.entities.Counseling;
+import xyz.sangdam.counseling.exceptions.CounselingNotFoundException;
 import xyz.sangdam.counseling.repositories.CounselingRepository;
 import xyz.sangdam.counseling.repositories.GroupCounselingRepository;
 import xyz.sangdam.counseling.repositories.PersonalCounselingRepository;
@@ -16,14 +18,33 @@ public class CounselingInfoService {
     private final GroupCounselingRepository groupRepository;
     private final CounselingRepository counselingRepository; // 목록 조회시
 
-    public Counseling get(Long cNo) {
+
+    public Counseling get(Long cNo) // 하나 조회 시
+    {
+        Counseling item = counselingRepository.findById(cNo).orElseThrow(CounselingNotFoundException::new);
+
+        if (CounselingType.PERSONAL == CounselingType.valueOf("personal")) {
+            personalRepository.findById(cNo).orElseThrow(CounselingNotFoundException::new);
+        }
+        return item;
+    }
+    // 목록 조회는 그룹상담만 해당
+    public ListData<Counseling> getList(CounselingSearch search)
+    {
+        int page = Math.max(search.getPage(), 1);
+        int limit = search.getLimit(); // 한페이지당 보여줄 레코드 개수
+        limit = limit < 1 ? 10 : limit;
+        int offset = (page -1) * limit; // 레코드 시작 위치 구하기
+
+        if (CounselingType.GROUP == CounselingType.valueOf("group")) {
+            groupRepository.findById()
+        }
+
+
         return null;
     }
 
-    public ListData<Counseling> getList(CounselingSearch search) {
-        return null;
-    }
-
+    // 2차가공
     private void addInfo(Counseling item) {
 
     }
