e24436883ac3e07d7cededa372b4a482afc9667d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-13 19:13:08 +0900

    RequestReservation

--- src/main/java/xyz/sangdam/counseling/controllers/RequestReservation.java
+++ src/main/java/xyz/sangdam/counseling/controllers/RequestReservation.java
@@ -8,13 +8,18 @@ import java.time.LocalTime;
 
 @Data
 public class RequestReservation {
-    private Long counselingNo;
-    private Long scheduleNo; //집단 상담, 개별 상담 X
 
-    /* 개별 상담 */
+    /* 집단 상담 */
+    private Long cNo; // 집단 상담 프로그램 번호
+
+    /* 개인 상담 */
+    private String category; // 개인 상담일 경우 상담 분류
+
     @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate rDate; // 예약일
 
     @JsonFormat(pattern="HH:mm")
-    private LocalTime rTime; // 예약시간
+    private LocalTime rTime; // 예약 시간
+
+    private String reason; // 상담 사유
 }
