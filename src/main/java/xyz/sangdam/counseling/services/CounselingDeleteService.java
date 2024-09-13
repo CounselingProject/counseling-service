99c8a238ba10fb7f9de2da62b56d0d867b60a20e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-13 20:47:14 +0900

    CounselingDeleteService : 상담 삭제 서비스

--- src/main/java/xyz/sangdam/counseling/services/CounselingDeleteService.java
+++ src/main/java/xyz/sangdam/counseling/services/CounselingDeleteService.java
@@ -2,18 +2,22 @@ package xyz.sangdam.counseling.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import xyz.sangdam.counseling.entities.Counseling;
 import xyz.sangdam.counseling.repositories.CounselingRepository;
-import xyz.sangdam.file.services.FileDeleteService;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class CounselingDeleteService {
 
-    private final CounselingRepository groupRepository;
-    private final FileDeleteService fileDeleteService;
+    private final CounselingRepository repository;
+    private final CounselingInfoService infoService;
 
-    public void delete(Long cNo) {
+    public void delete(Long cNo) { // 외래키 제약 조건 때문에 완전 삭제가 불가능하므로 deletedAt 을 사용
+        Counseling data = infoService.get(cNo);
+        data.setDeletedAt(LocalDateTime.now());
 
-        // fileDeleteService.delete(gid);
+        repository.saveAndFlush(data); // data 저장
     }
 }
