a1baa425756c917b0cd2262a6bf14b7a934220c7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-13 19:02:36 +0900

    RequestCounseling

--- src/main/java/xyz/sangdam/counseling/controllers/RequestCounseling.java
+++ src/main/java/xyz/sangdam/counseling/controllers/RequestCounseling.java
@@ -1,9 +1,44 @@
 package xyz.sangdam.counseling.controllers;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RequestCounseling {
-    private Long counselingNo;
-    private String counselingType;
+
+    private Long cNo; // 상담 번호 : 수정 시 필요
+
+    private String mode; // write - 등록, update - 수정
+
+    @NotBlank
+    private String gid; // 이미지 등록에 필요
+
+    @NotBlank
+    private String counselingDes; // 상담 설명
+
+    @NotBlank
+    private String counselorName; // 상담사명
+
+    @NotBlank
+    private String counselorEmail; // 상담사 이메일
+
+    @NotBlank
+    private String counselingName; // 집단 상담 프로그램명
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate reservationSdate; // 신청 기간 시작 일시
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate reservationEdate; // 신청 기간 종료 일시
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime counselingDate; // 상담일
+
+    private int counselingLimit; // 정원
 }
