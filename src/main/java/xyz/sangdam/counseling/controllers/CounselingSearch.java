7b26664fa3383673abe6f4256058d8335d93471e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-13 19:53:03 +0900

    CounselingSearch

--- src/main/java/xyz/sangdam/counseling/controllers/CounselingSearch.java
+++ src/main/java/xyz/sangdam/counseling/controllers/CounselingSearch.java
@@ -5,5 +5,4 @@ import xyz.sangdam.global.CommonSearch;
 
 @Data
 public class CounselingSearch extends CommonSearch {
-    private String counselingType; // InfoService 의 GetList - 개인, 집단, 전체 프로그램 목록 조회에서 사용
 }
