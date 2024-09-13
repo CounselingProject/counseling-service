084341dafd7624b68ac82a48cd2ded575aad2cc8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-13 19:53:18 +0900

    ReservationApplyService

--- src/main/java/xyz/sangdam/counseling/services/ReservationApplyService.java
+++ src/main/java/xyz/sangdam/counseling/services/ReservationApplyService.java
@@ -1,21 +1,12 @@
+
 package xyz.sangdam.counseling.services;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import xyz.sangdam.counseling.controllers.RequestReservation;
-import xyz.sangdam.counseling.repositories.GroupReservationRepository;
-import xyz.sangdam.counseling.repositories.ReservationRepository;
-import xyz.sangdam.member.MemberUtil;
 
 @Service
-@RequiredArgsConstructor
 public class ReservationApplyService {
 
-    private final ReservationRepository reservationRepository; // 개인 상담
-    private final GroupReservationRepository groupReservationRepository; // 집단 상담
-    private final MemberUtil memberUtil;
+    public void apply() {
 
-    public void apply(RequestReservation form) {
-        
     }
 }
