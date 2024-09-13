def4551aaa1873553ffe1991d08f42c0cb94ebe3
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-13 18:59:56 +0900

    Reservation

--- src/main/java/xyz/sangdam/counseling/entities/Reservation.java
+++ src/main/java/xyz/sangdam/counseling/entities/Reservation.java
@@ -2,29 +2,38 @@ package xyz.sangdam.counseling.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import xyz.sangdam.counseling.constants.CounselingType;
+import xyz.sangdam.counseling.constants.PersonalCategory;
 import xyz.sangdam.counseling.constants.Status;
 import xyz.sangdam.global.entities.BaseEntity;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation extends BaseEntity {
-    @Id @GeneratedValue
-    private Long reservationNo;
+    @Id
+    @GeneratedValue
+    private Long rNo; // 예약 접수 번호
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="counselingNo")
-    private Counseling counseling; // CounselingType으로 개인/집단상담 구분
+    @Column(length=20)
+    @Enumerated(EnumType.STRING)
+    private Status status; // 진행 상태
 
     @Enumerated(EnumType.STRING)
-    @Column(length = 20)
-    private Status status; // 진행상태
+    @Column(length=20)
+    private CounselingType counselingType; // 개인, 집단 상담 구분
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-    private LocalDateTime reservationDate; // 예약일시
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="cNo")
+    private Counseling counseling; // 집단 상담
 
     @Column(length = 40, nullable = false)
     private String userName; // 로그인 회원명
@@ -32,9 +41,25 @@ public class Reservation extends BaseEntity {
     @Column(length = 80, nullable = false)
     private String email; // 로그인 회원 이메일
 
+    @Enumerated(EnumType.STRING)
+    @Column(length=20)
+    private PersonalCategory category; // 개인 상담 종류
+
+    @Column(length=60, nullable = false)
+    private String counselingName; // 개인 상담명(학생명(학번)님 개인 상담) + 집단 상담 프로그램명
+
+    @Column(length=20, nullable = false)
+    private String counselorName; // 상담사명
+
+    @Column(length=65, nullable = false)
+    private String counselorEmail; // 상담사 이메일
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime rDateTime; // 예약 일시
+
     @Lob
     private String reason; // 상담 사유
 
     @Lob
-    private String record; // 상담 일지(학생별)
+    private String record; // 상담 일지
 }
