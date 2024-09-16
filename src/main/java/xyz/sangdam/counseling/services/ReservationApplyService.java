fa0c2673aa3263fed0ff5a1e4d2354a5c14a4fbe
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-16 16:20:53 +0900

    ReservationApplyService 작업 시작

--- src/main/java/xyz/sangdam/counseling/services/ReservationApplyService.java
+++ src/main/java/xyz/sangdam/counseling/services/ReservationApplyService.java
@@ -2,11 +2,12 @@
 package xyz.sangdam.counseling.services;
 
 import org.springframework.stereotype.Service;
+import xyz.sangdam.counseling.controllers.RequestReservation;
 
 @Service
 public class ReservationApplyService {
 
-    public void apply() {
-
+    public void apply(RequestReservation form) {
+        Long cNo = form.getCNo();
     }
 }
