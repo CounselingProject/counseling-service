1db6d2d6dcac119fbdffd788eb4fc5983fbfbba1
sunkyu0384 <sunkyu0384@naver.com>
2024-09-25 18:32:21 +0900

    .

--- src/main/java/xyz/sangdam/counseling/controllers/RequestReservation.java
+++ src/main/java/xyz/sangdam/counseling/controllers/RequestReservation.java
@@ -1,12 +1,14 @@
 package xyz.sangdam.counseling.controllers;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RequestReservation {
 
     /* 집단 상담 */
